0e23b69bd32256a064da5bcb9062167db2a3b68e
lunarji <2415970940@qq.com>
2018-07-08 18:08:29 +0800

    index.html  highchats  seven days  read num

--- read_statistics/utils.py
+++ read_statistics/utils.py
@@ -1,4 +1,6 @@
+import datetime
 from .models import Readnum,ReadDetail
+from django.db.models import Sum
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import F
 from django.utils import timezone
@@ -7,22 +9,25 @@ def read_statistics_once_read(request,obj):
     ct = ContentType.objects.get_for_model(obj)
     key = "%s_%s_read"%(ct.model,obj.pk)
     if not request.COOKIES.get(key):
-
-        # if Readnum.objects.filter(content_type=ct,object_id=obj.pk).count():
-        #     readnum=Readnum.objects.filter(content_type=ct,object_id=obj.pk)
-        # else:
-        #     readnum=Readnum.objects.create(content_type=ct,object_id=obj.pk)
         readnum,created =Readnum.objects.get_or_create(content_type=ct,object_id=obj.pk)
         readnum.read_num+=1
         readnum.save()
         # readnum.update(read_num=F('read_num')+1)
 
         today = timezone.now().date()
-        # if ReadDetail.objects.filter(content_type=ct,object_id=obj.pk,date=today).count():
-        #     readdetail=ReadDetail.objects.get(content_type=ct,object_id=obj.pk,date=today)
-        # else:
-        #     readdetail=ReadDetail.objects.create(content_type=ct,object_id=obj.pk,date=today)
         readdetail,created=ReadDetail.objects.get_or_create(content_type=ct,object_id=obj.pk,date=today)
         readdetail.read_num+=1
         readdetail.save()
     return key
+
+def seven_read(contenttype):
+    today = timezone.now().date()
+    read_num=[]
+    dates = []
+    for i in range(6,-1,-1):
+        date = today - datetime.timedelta(days=i)
+        dates.append(date.strftime("%m/%d"))
+        readdetail = ReadDetail.objects.filter(content_type=contenttype,date=date)
+        result = readdetail.aggregate(read_sum = Sum('read_num'))
+        read_num.append(result['read_sum'] or 0)
+    return read_num,dates
