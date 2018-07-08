3f5da648acefd86216161e124339894d7a4cab4b
lunarji <2415970940@qq.com>
2018-07-08 16:15:39 +0800

    use get_or_create

--- read_statistics/utils.py
+++ read_statistics/utils.py
@@ -8,19 +8,21 @@ def read_statistics_once_read(request,obj):
     key = "%s_%s_read"%(ct.model,obj.pk)
     if not request.COOKIES.get(key):
 
-        if Readnum.objects.filter(content_type=ct,object_id=obj.pk).count():
-            readnum=Readnum.objects.filter(content_type=ct,object_id=obj.pk)
-        else:
-            readnum=Readnum.objects.create(content_type=ct,object_id=obj.pk)
+        # if Readnum.objects.filter(content_type=ct,object_id=obj.pk).count():
+        #     readnum=Readnum.objects.filter(content_type=ct,object_id=obj.pk)
+        # else:
+        #     readnum=Readnum.objects.create(content_type=ct,object_id=obj.pk)
+        readnum,created =Readnum.objects.get_or_create(content_type=ct,object_id=obj.pk)
         readnum.read_num+=1
         readnum.save()
         # readnum.update(read_num=F('read_num')+1)
 
         today = timezone.now().date()
-        if ReadDetail.objects.filter(content_type=ct,object_id=obj.pk,date=today).count():
-            readdetail=ReadDetail.objects.get(content_type=ct,object_id=obj.pk,date=today)
-        else:
-            readdetail=ReadDetail.objects.create(content_type=ct,object_id=obj.pk,date=today)
+        # if ReadDetail.objects.filter(content_type=ct,object_id=obj.pk,date=today).count():
+        #     readdetail=ReadDetail.objects.get(content_type=ct,object_id=obj.pk,date=today)
+        # else:
+        #     readdetail=ReadDetail.objects.create(content_type=ct,object_id=obj.pk,date=today)
+        readdetail,created=ReadDetail.objects.get_or_create(content_type=ct,object_id=obj.pk,date=today)
         readdetail.read_num+=1
         readdetail.save()
     return key
